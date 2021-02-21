77c954bcc254fabbfbea61bdbc67c4ee746e8a34
nuriddin-islamov <75730842+nuriddin-islamov@users.noreply.github.com>
2021-02-22 00:01:02 +0500

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,3 +1,3 @@
 API_TOKEN = '1630933719:AAEcRVcOaKJU5T7IEtWsS_YCXvJ8oRbKU-M'
-database_path = "/Users/nuriddin07/Documents/dev/PycharmProjects/TVSALE_BOT/tvsale.sqlite"
+database_path = "tvsale.sqlite"
 
